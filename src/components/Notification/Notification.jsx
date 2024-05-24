81df36380d06d23c3d0c4b56e57679d33bb700a3
Yilchik <yulyayulia2@gmail.com>
2024-05-24 17:13:36 +0200

    instal

--- src/components/Notification/Notification.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./Notification.module.css";
-
-const Notification = ({ message }) => {
-  return <p className={css.notification}>{message}</p>;
-};
-
-export default Notification;
