5e0f45248cd47e9cc697dc59e8fa69316dedf17a
Yilchik <yulyayulia2@gmail.com>
2024-05-24 20:54:17 +0200

    SearchBox

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import "./App.css";
 import primaryContacts from "./contacts.json";
 import { useState } from "react";
 import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 function App() {
   const [contacts] = useState(primaryContacts);
-  // const addContact = () => {};
+  const [filter, setFilter] = useState("");
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <>
       <div>
         <h1>Phonebook</h1>
-        {/* <ContactForm />
-        <SearchBox /> */}
-        <ContactList contacts={contacts} />
+        {/* <ContactForm /> */}
+        <SearchBox value={filter} onFilter={setFilter} />
+        <ContactList contacts={visibleContacts} />
       </div>
     </>
   );
